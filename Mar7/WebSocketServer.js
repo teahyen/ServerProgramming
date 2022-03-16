f8b9bfddc9dd68ed30fe1d676390542f2e6bc8a5
HanUseGentoo <wooyeophan@gmail.com>
2022-03-16 15:27:17 +0900

    3월 16일 과제

--- Mar7/WebSocketServer.js
+++ Mar7/WebSocketServer.js
@@ -1,23 +1,31 @@
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 48000 }, () => {
+const wss = new WebSocket.Server({ port: process.env.PORT || 48000 }, () => {
     console.log("Server is running on port 48000");
 });
 
 wss.on("connection", socket => {
     console.log("New client connected");
+    socket.send("Install Gentoo");
 
     socket.on("message", data => {
         console.log(data.toString());
-        socket.send(data + " Echo");
+        socket.send("Client: %s", data);
     });
 
     socket.on("close", data => {
         console.log("Client disconnected");
     });
+
+
+    socket.interval = setInterval(() => { // 서버 시작과 동시에 계속 실행함 (클라이언트 없어도)
+        if (socket.readyState == socket.OPEN) { // 연결이 성사된 소켓에게만 보내줌
+            socket.send("INSTALL GENTOO");
+        }
+    }, 3000);
 });
 
 
 wss.on("listening", () => {
-    console.log("Server listening on port 48000");
+    console.log(`Server listening on port ${process.env.PORT || 48000}`);
 });
--- ServerProgrammingMarch7/Assets/_Scripts/WebSocketTalker.cs
+++ ServerProgrammingMarch7/Assets/_Scripts/WebSocketTalker.cs
@@ -1,17 +1,29 @@
+using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using UnityEngine;
 using WebSocketSharp;
 
 public class WebSocketTalker : MonoBehaviour
 {
-    WebSocket ws;
+    private readonly ConcurrentQueue<Action> _queue = new ConcurrentQueue<Action>(); // Concurrent 는 Thread Safe 함
+    // 동시에 접근할때 dequeue 보다 enqueue 가 먼저 일어남 (보장됨)
+
+    // const string URL = "ws://ggmhan.herokuapp.com/";
+    const string URL = "ws://127.0.0.1:48000";
+    private WebSocket ws;
+
+    public GameObject cubeObj;
 
     private void Start() {
-        ws = new WebSocket("ws://127.0.0.1:48000");
+        ws = new WebSocket(URL);
 
         ws.OnMessage += (sender, e) => {
-            Debug.Log($"Message received from: {(sender as WebSocket).Url}, Data: {e.Data}");
+            if(!string.IsNullOrEmpty(e.Data)) _queue.Enqueue(GenerateCube);
+
+            Debug.Log($"Message received from: {(sender as WebSocket).Url}\r\nData: {e.Data}");
+            
         };
 
         ws.Connect();
@@ -22,9 +34,36 @@ public class WebSocketTalker : MonoBehaviour
             return;
         }
 
+        if(Input.GetKeyDown(KeyCode.A)) {
+            Debug.Log("Connection closed at client");
+            ws.Close();
+        }
+
+        if(Input.GetKeyDown(KeyCode.C)) {
+            if(ws.IsAlive) return;
+            Debug.Log("Connection established");
+            ws.Connect();
+        }
+
         if(Input.GetKeyDown(KeyCode.Space)) {
             ws.Send("Hello");
         }
+
+
+        while (_queue.Count > 0) {
+            if(_queue.TryDequeue(out var e)) {
+                e?.Invoke();
+            } else {
+                break;
+            }
+        }
+    }
+
+    float x = 0;
+    private void GenerateCube()
+    {
+        Instantiate(cubeObj, new Vector3(x += 0.5f, 0.0f, 0.0f), Quaternion.identity).GetComponent<Rigidbody>().AddForce(new Vector3(UnityEngine.Random.Range(-1.0f, 1.0f), 10.0f, UnityEngine.Random.Range(-1.0f, 1.0f)), ForceMode.Impulse);
+        Debug.Log("Generated");
     }
 
 
